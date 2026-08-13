f9453a747e2347d5dbf4b42a3bfef7e9d6966a9e
MakuMutts <kazminaviktoriya@gmail.com>
2026-08-13 19:31:12 +0300

    finally built

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/kazmi/Task_1/Task_1
+# Build directory: C:/Users/kazmi/Task_1/Task_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/kazmi/Task_1/Task_1/CMakeLists.txt;89;add_test;C:/Users/kazmi/Task_1/Task_1/CMakeLists.txt;0;")
